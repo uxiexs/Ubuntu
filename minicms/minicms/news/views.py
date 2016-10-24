8739ba87be4ae7c2dd17acbe39065a240a517464
uxeix <uxeixs@aliyun.com>
2016-10-24 16:55:06 +0800

    增加base.html模板/article_slug.html/column_slug.html 修改index.html

--- minicms/minicms/news/views.py
+++ minicms/minicms/news/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,'news/re.html')
+    return render(request,'news/index.html')
 
 def column_detail(request, column_slug):
     return render(request,'news/column_detail.html',{'column_slug': column_slug})
 
 def article_detail(request, article_slug):
-    return render(request,'news/article_detial.html', {'article_slug': article_slug})
+    return render(request,'news/article_detail.html', {'article_slug': article_slug})
