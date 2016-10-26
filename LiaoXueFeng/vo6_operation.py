3aace0df71c8147f4589683fcf68328e00ccda52
uxeix <uxeixs@aliyun.com>
2016-10-26 15:27:06 +0800

    python内数据的数学运算

---
+++ LiaoXueFeng/vo6_operation.py
@@ -0,0 +1,8 @@
+#/usr/bin/python3
+# coding:utf-8
+print('+:', 1 + 1)  # 加
+print('-', 3 - 5)   # 减
+print('*', 25 * 25) # 乘
+print(10 // 3)      # 除
+print(10 / 3.0)     # 整除
+print(10 % 3)       # 取余
