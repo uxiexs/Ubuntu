9bebf2434ed275d1bf659fd2a0fd19ba5d250ada
uxeix <uxeixs@aliyun.com>
2016-11-01 13:53:35 +0800

    可迭代对象

--- LiaoXueFeng/vo20_iter.py
+++ LiaoXueFeng/vo20_iter.py
@@ -1,10 +1,9 @@
 #/usr/bin/python3
 # coding:utf-8
-
+from collections import Iterable
 #*************************************************************************
 # 判断一个对象是否是Iterable对象
 #*************************************************************************
-from collections import Iterable
 
 
 def isIterable(e):
@@ -12,15 +11,15 @@ def isIterable(e):
 
 print('List属于可迭代对象:', isIterable([]))  # list
 print('dict属于可迭代对象:', isIterable({}))  # dict
-print('set属于可迭代对象:', isIterable(set(['a', 'b', 'c']))) # set
-print('tuple属于可迭代对象:',isIterable(('U','x','e','i'))) # tuple
+print('set属于可迭代对象:', isIterable(set(['a', 'b', 'c'])))  # set
+print('tuple属于可迭代对象:', isIterable(('U', 'x', 'e', 'i')))  # tuple
 print('str属于可迭代对象:', isIterable('abc'))  # str
-print('generator属于可迭代对象:',isIterable(x**x for x in range(10))) #generator
-print('number属于可迭代对象:', isIterable(100)) #number
+print('generator属于可迭代对象:', isIterable(x**x for x in range(10)))  # generator
+print('number属于可迭代对象:', isIterable(100))  # number
 
-#*******************************************************************************
+#*************************************************************************
 # 我们已经知道，可以直接作用于for循环的数据类型有以下几种：
 # 一类是集合数据类型，如list、tuple、dict、set、str等；
 # 一类是generator，包括生成器和带yield的generator function。
 # 这些可以直接作用于for循环的对象统称为可迭代对象：Iterable。
-#*******************************************************************************
+#*************************************************************************
