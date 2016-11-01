e17b433be576a3487c8cdacbe05449d29b50b776
uxeix <uxeixs@aliyun.com>
2016-11-01 13:54:50 +0800

    迭代器

--- LiaoXueFeng/vo21_itertor.py
+++ LiaoXueFeng/vo21_itertor.py
@@ -14,5 +14,10 @@ print('dict属于迭代器:', isIterator({}))  # dict
 print('set属于迭代器:', isIterator(set(['a', 'b', 'c'])))  # set
 print('tuple属于迭代器:', isIterator(('U', 'x', 'e', 'i')))  # tuple
 print('str属于迭代器:', isIterator('abc'))  # str
-print('generator属于迭代器:', isIterator(x**x for x in range(10))) #generator
+print('generator属于迭代器:', isIterator(x**x for x in range(10)))  # generator
 print('number属于迭代器:', isIterator(100))  # number
+
+#*************************************************************************
+# 生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator。
+# 把list、dict、str等Iterable变成Iterator可以使用iter()函数：
+#*************************************************************************
