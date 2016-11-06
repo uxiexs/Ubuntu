2d156fe498e9101bea877b1682e5e23daead3561
Uxeix <uxiex@aliyun.com>
2016-11-06 21:30:04 +0800

    我爱王迅洋

---
+++ LiaoXueFeng/vo36_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# encoding: utf-8
+"""
+@version: 3.52
+@author: Uxeix
+@license: Apache Licence 
+@contact: Uxeixs@gmail.com
+@site: https://github.com/uxiexs
+@software: PyCharm
+@file: vo36_class.py.py
+@time: 2016/11/6 21:25
+"""
