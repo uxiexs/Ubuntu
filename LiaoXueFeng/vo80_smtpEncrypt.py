5b65f69d0a8b0825cbb3b042c63ede0668c1022d
uxeix <uxeixs@aliyun.com>
2016-11-25 16:16:50 +0800

    SMTP 发送加密邮件

--- LiaoXueFeng/vo80_smtpEncrypt.py
+++ LiaoXueFeng/vo80_smtpEncrypt.py
@@ -34,8 +34,8 @@ from_addr = 'uxeixs@126.com'
 passwd = '870125xx'
 
 # 接收参数: 收件人地址,可多个
-to_addr = input('收件人邮箱: ')
-# to_addr = 'uxeixs@gmail.com'
+# to_addrs = ['uxeixs@gmail.com','koulaxx@163.com']
+to_addrs = 'uxeixs@gmail.com'
 
 # 接收参数: SMTP服务器(注意:是发件人的smtp服务器)
 smtp_server = 'smtp.126.com'
@@ -48,17 +48,17 @@ subject = input('邮件主题: ')
 # 指定subtype为alternative，同时支持html和plain格式
 msg = MIMEMultipart('alternative')
 # 邮件正文中显示图片，同时附件的图片将不再显示
-plain = 'Hello world and hello me!'
-msg.attach(MIMEText(plain, 'plain', 'utf-8'))       # 纯文本
+# plain = 'Hello world and hello me!'
+# msg.attach(MIMEText(plain, 'plain', 'utf-8'))       # 纯文本
 html = '<html><body><h1>Hello</h1><p><img src="cid:0"></p></body></html>'
 msg.attach(MIMEText(html, 'html', 'utf-8'))         # HTML
-# 添加附件：即关联一个MIMEBase，图片为本地读取
-with open('/home/uxeix/Pictures/Screenshot from 2016-11-21 15-26-55.png', 'rb') as f:
+#添加附件：即关联一个MIMEBase，图片为本地读取
+with open('/home/uxeix/Pictures/icon/favicon (Jianshu).ico', 'rb') as f:
     # 设置附件中的MIME和文件名
-    mime = MIMEBase('image', 'png', filename='Hatsune Miku.png')
+    mime = MIMEBase('image', 'jpeg', filename='hot.jpg')
     # 加上必要的头信息
     mime.add_header('Content-Disposition', 'attachment',
-                    filename='Hatsune Miku.png')
+                    filename='hot.jpg')
     mime.add_header('Content-ID', '<0>')
     mime.add_header('X-Attachment-Id', '0')
     # 把附件的内容读进来
@@ -70,7 +70,8 @@ with open('/home/uxeix/Pictures/Screenshot from 2016-11-21 15-26-55.png', 'rb') 
 
 # 未指定用户别名，则客户端会自动提取邮件地址中的名称作为邮件的用户别名
 msg['From'] = _format_addr(from_addr)
-msg['To'] = _format_addr(to_addr)
+msg['To'] = _format_addr(to_addrs)
+# msg['To'] = '%s' % ','.join([_format_addr('<%s>' % to_addr) for to_addr in to_addrs])
 msg['Subject'] = Header(str(subject), 'utf-8').encode()
 msg['Date'] = formatdate()
 
@@ -86,7 +87,7 @@ try:
     server.login(from_addr, passwd)
 
     # 发送邮件
-    server.sendmail(from_addr, [to_addr], msg.as_string())
+    server.sendmail(from_addr, [to_addrs], msg.as_string())
 
     print('邮件发送成功')
 
