ba1ae9911084e2b6128b892008faa2b20b8817be
Uxeix <uxiex@aliyun.com>
2016-12-04 17:14:42 +0800

    在字典上对数据执行各式各样的结算（比如求最小值、最大值、排序等）

--- python-cookbook/src/1/calculating_with_dictionaries/example.py
+++ python-cookbook/src/1/calculating_with_dictionaries/example.py
@@ -3,11 +3,11 @@
 # Example of calculating with dictionaries
 
 prices = {
-   'ACME': 45.23,
-   'AAPL': 612.78,
-   'IBM': 205.55,
-   'HPQ': 37.20,
-   'FB': 10.75
+    'ACME': 45.23,
+    'AAPL': 612.78,
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
 }
 
 # Find min and max price
@@ -21,5 +21,3 @@ print('sorted prices:')
 prices_sorted = sorted(zip(prices.values(), prices.keys()))
 for price, name in prices_sorted:
     print('    ', name, price)
-
-
