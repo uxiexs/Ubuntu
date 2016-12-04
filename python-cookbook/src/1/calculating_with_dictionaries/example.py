0d32cd9f9cbe022678f38dbc7f9c82bd8f8a4547
Uxeix <uxiex@aliyun.com>
2016-12-04 17:25:52 +0800

    在字典上对数据执行各式各样的结算（比如求最小值、最大值、排序等）

--- python-cookbook/src/1/calculating_with_dictionaries/example.py
+++ python-cookbook/src/1/calculating_with_dictionaries/example.py
@@ -1,5 +1,5 @@
-# example.py
-#
+#!/usr/bin/python3
+# coding:utf-8
 # Example of calculating with dictionaries
 
 prices = {
@@ -11,6 +11,7 @@ prices = {
 }
 
 # Find min and max price
+# 为了对字典内容做运算，利用zip()将字典的键值反转。
 min_price = min(zip(prices.values(), prices.keys()))
 max_price = max(zip(prices.values(), prices.keys()))
 
@@ -18,6 +19,13 @@ print('min price:', min_price)
 print('max price:', max_price)
 
 print('sorted prices:')
+# 对数据排序只要使用zip()再配合sorted()就可以了。
 prices_sorted = sorted(zip(prices.values(), prices.keys()))
 for price, name in prices_sorted:
     print('    ', name, price)
+    # =======================================================================================================
+    # 注意：zip()将创建一个迭代器，它的内容只能被消费一次：
+    # prices_and_names = zip(price.values(), price.keys())
+    # print（min(prices_and_names)) # OK
+    # print(mzx(prices_and_names)) # ValueError: max() is an empty sequence
+    # =======================================================================================================
